57dc4dce93377d55b206d17313ddc2f95a8a7160
maxwell <m.nelson.study@gmail.com>
2017-03-26 18:05:23 -0700

    added login button to index page and setup server to process registration form

--- main/apps/adminPortal/urls.py
+++ main/apps/adminPortal/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^admin_portal', views.portal, name='admin_portal'),
+	url(r'^register_customer', views.cRegister, name='register'),
 	url(r'^admin_validate', views.validateAdmin, name='admin_validate'),
 	url(r'^business_dashboard', views.b_dash, name='b_dash'),
 ]
--- main/apps/adminPortal/views.py
+++ main/apps/adminPortal/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from models import User
+from models import User, SubCompany
+from pprint import pprint
 
 # Create your views here.
 def portal(request):
-	return render(request, 'adminPortal/account.html')
+	return render(request, 'adminPortal/cAccount.html')
 
 def validateAdmin(request):
 	print(request.POST['userEmail'])
@@ -19,3 +20,13 @@ def validateAdmin(request):
 
 def b_dash(request):
 	return render(request, 'adminPortal/businessDashboard.html')
+
+def cRegister(request):
+	print(request.POST['cname'])
+	print(request.POST['cemail'])
+	print(request.POST['cpass'])
+	print(request.POST['ccpass'])
+	print(request.POST['ctype'])
+	company = SubCompany(name=request.POST['cname'], email=request.POST['cemail'], password=request.POST['cpass'])
+	company.save()
+	return redirect('account:b_dash')
